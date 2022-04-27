2e8eedd52bde5453f5456743723c951ce40e71ed
Hannah Callison <hannahcallison1007@gmail.com>
2022-04-27 14:47:35 -0700

    adding code

---
+++ Develop/routes/diagnostics.js
@@ -0,0 +1,30 @@
+const diagnostics = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving diagnostic information
+diagnostics.get('/', (req, res) => {
+  // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/diagnostics.json').then((data) =>res.json(JSON.parse(data)))
+});
+
+// POST Route for a error logging
+diagnostics.post('/', (req, res) => {
+  // TODO: Logic for appending data to the db/diagnostics.json file
+  console.log(req.body);
+
+  if (req.body){
+    const newDiag = {
+      time: Date.now(), 
+      error_id: uuidv4(),
+      errors: req.body
+    };
+
+    readAndAppend(newDiag, './db/diagnostics.json');
+    res.json(req.body);
+  } else {
+    res.error('Error in adding diagnostic')
+  }
+});
+
+module.exports = diagnostics;
